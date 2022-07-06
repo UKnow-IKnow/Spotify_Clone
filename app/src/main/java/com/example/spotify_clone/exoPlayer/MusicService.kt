aafe795155a29ffc542da452fa8094b21eae5f37
UKnow-IKnow <sarojsahu014@gmail.com>
2022-07-06 22:11:39 +0530

    adding media stuff

--- app/src/main/java/com/example/spotify_clone/exoPlayer/MusicService.kt
+++ app/src/main/java/com/example/spotify_clone/exoPlayer/MusicService.kt
@@ -1,10 +1,63 @@
 package com.example.spotify_clone.exoPlayer
 
+import android.app.PendingIntent
+import android.media.session.MediaSession
 import android.os.Bundle
+import android.service.media.MediaBrowserService
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import java.util.*
+import javax.inject.Inject
 
+
+private const val SERVICE_TAG = "MusicService"
+
+@AndroidEntryPoint
 class MusicService: MediaBrowserServiceCompat() {
+
+    @Inject
+    lateinit var dataSourceFactory: DefaultMediaSourceFactory
+
+    @Inject
+    lateinit var exoPlayer: SimpleExoPlayer
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
+
+    override fun onCreate() {
+        super.onCreate()
+        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this, 0, it, 0)
+        }
+
+        mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
+            setSessionActivity(activityIntent)
+            isActive = true
+        }
+
+        sessionToken = mediaSession.sessionToken
+
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector.setPlayer(exoPlayer)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
 
     override fun onGetRoot(
         clientPackageName: String,
